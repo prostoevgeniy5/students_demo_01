24909ad19b7e76d95cd5fbf1818749191c2916b6
evgeniy <yevgeniy5@ukr.net>
2022-06-13 21:41:03 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// const express = require('express')
+import express, {Request, Response} from 'express'
+const app = express()
+const port = process.env.PORT || 3000
+
+const videos = [
+  {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
+  {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
+  {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
+  {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
+  {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
+]  
+
+app.get('/', (req: Request , res: Response) => {
+   // debugger;
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
