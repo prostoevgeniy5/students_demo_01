841c7d26ea0b2853e56840ff53722c760339dff9
evgeniy <yevgeniy5@ukr.net>
2022-06-24 13:01:50 +0300

    Created REST API for bloggers and posts

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,8 @@ let videos = [
 
 let bloggers = [
   {"id": 0, "name": "Mark Solonin", "youtubeUrl": "https://www.youtube.com/channel/UChLpUGaZO35ICTltBP50VSg"}, 
-  {"id": 1, "name": "Dmitry Robionek", "youtubeUrl": "https://www.youtube.com/user/ideafoxvideo"}
+  {"id": 1, "name": "Dmitry Robionek", "youtubeUrl": "https://www.youtube.com/user/ideafoxvideo"},
+  {"id": 2, "name": "Dmitry", "youtubeUrl": "https://www.youtube.com/c/ITKAMASUTRA"}
 ]
 
 let posts = [
@@ -26,16 +27,58 @@ let posts = [
     "shortDescription": "Message for dyadya",
     "content": "Moy dyadya samih chesnih pravil, kogda ne v shutku sanemog, on uvajat sebya sastavil i luchshe vidumat ne smog.",
     "bloggerId": 0,
-    "bloggerName": "Vasya"},
+    "bloggerName": "Mark Solonin"},
   {
     "id": 1,
+    "title": "Solomennaya shlyapka",
+    "shortDescription": "Libetta, Lizetta, Myuzetta",
+    "content": "Либетта, Лизетта, Мюзетта, Жонетта, Жоржетта. Вся жизнь моя вами как солнцем июньским согрета...",
+    "bloggerId": 2,
+    "bloggerName": "Dmitry"
+  },
+  {
+    "id": 2,
     "title": "T G Shevchenko",
     "shortDescription": "Message for oligarhs",
     "content": "Якби ви знали, паничі, Де люде плачуть живучи, То ви б елегій не творили Та марне Бога б не хвалили",
-    "bloggerId": 2,
-    "bloggerName": "Andriy"
+    "bloggerId": 1,
+    "bloggerName": "Dmitry Robionek"
   }
 ]
+
+type errorsDescription = {
+  message: string
+  field: string
+}
+
+interface errorsType {
+  errorsMessages: errorsDescription[]
+}
+
+function checkRequestBodyField (name: string, field: string): boolean {
+  let result = false;
+  if(field === 'name'){
+    result = name === undefined || typeof name !== 'string' || name.trim() === '' || name.length > 15 ? true : false;
+  } else if (field === "youtubeUrl") {
+    result = name === undefined || typeof name !== 'string' || name.trim() === '' || name.length > 100 || !/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/.test(name) ? true : false;
+  } else if (field === 'title') {
+    result = name === undefined || typeof name !== 'string' || name.trim() === '' || name.length > 30 ? true : false;
+  } else if (field === 'shortDescription') {
+    result = name === undefined || typeof name !== 'string' || name.trim() === '' || name.length > 100 ? true : false;
+  } else if (field === 'content') {
+    result = name === undefined || typeof name !== 'string' || name.trim() === '' || name.length > 1000 ? true : false;
+  }  else if (field === 'bloggerId') {
+    let blogger = bloggers.find(item => +item.id === +name)
+    result = typeof +name !== 'number' || !blogger ? true : false;
+  } 
+  return result
+}
+
+function errorFields():errorsType {
+  return {
+    errorsMessages: []
+  }
+}
 
 const bodyparser = bodyParser({})
 app.use(bodyparser)
@@ -51,7 +94,6 @@ app.get('/videos', (req: Request , res: Response) => {
 })
 
 app.get('/videos/:id', (req: Request , res: Response) => {
-  console.log(req.query, 'req.query', req.params, 'req.params')
   let videoItem = videos.find( item => item.id === +req.params.id )
   if (videoItem) {
     res.send(videoItem);
@@ -73,7 +115,6 @@ app.delete('/videos/:id', (req: Request , res: Response) => {
 })
 
 app.post('/videos', (req: Request , res: Response) => {
-  console.log(req.body)
   let title = req.body.title 
   if(title === undefined || typeof title !== 'string' || title.trim() === '' || title.length > 40) {
     res.status(400).send({  
@@ -102,11 +143,9 @@ app.put('/videos/:id', (req: Request , res: Response) => {
     if(item.id === +req.params.id) {
       index = ind
     } return item.id === +req.params.id })
-  console.log(videoItem)
   if (videoItem) {
     let title = req.body.title;
     if(title === undefined || typeof title !== 'string' || title.trim() === '' || title.length > 40) {
-      console.log(req.body.title)
       res.status(400).send({
         "errorsMessages": [
           {
@@ -118,8 +157,7 @@ app.put('/videos/:id', (req: Request , res: Response) => {
       return
     }
     videoItem.title = req.body.title
-    videoItem.author = 'it-incubator.eu'
-    console.log(videoItem)    
+    videoItem.author = 'it-incubator.eu'  
     res.sendStatus(204).send(videoItem);
   } else {
     res.sendStatus(404)
@@ -132,7 +170,6 @@ app.get('/bloggers', (req: Request , res: Response) => {
 })
 
 app.get('/bloggers/:id', (req: Request , res: Response) => {
-  console.log(req.query, 'req.query', req.params, 'req.params')
   let bloggerItem = bloggers.find( item => item.id === +req.params.id )
   if (bloggerItem) {
     res.status(200).send(bloggerItem);
@@ -142,69 +179,25 @@ app.get('/bloggers/:id', (req: Request , res: Response) => {
 })
 
 app.delete('/bloggers/:id', (req: Request , res: Response) => {
-    const newVideos = videos.filter(item => {
+  let length = bloggers.length
+    bloggers = bloggers.filter(item => {
     return item.id !== Number.parseInt(req.params.id)
   })
-  if(videos.length > newVideos.length) {
-    videos = newVideos;
+  if(length > bloggers.length) {
     res.send(204)
-  } else if(videos.length === newVideos.length) {
+  } else if(length === bloggers.length) {
     res.send(404)
   }
 })
 
-type errorsDescription = {
-  message: string
-  field: string
-}
-
-interface errorsType {
-  errorsMessages: errorsDescription[]
-}
-
-function checkRequestBodyField (name: string, field: string): boolean {
-  let result = false;
-  if(field === 'name'){
-    result = name === undefined || typeof name !== 'string' || name.trim() === '' || name.length > 15 ? true : false;
-  } else if (field === "youtubeUrl") {
-    result = name === undefined || typeof name !== 'string' || name.trim() === '' || name.length > 100 || !/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/.test(name) ? true : false;
-  }
-  return result
-}
-
-function errorFields():errorsType {
-  return {
-    errorsMessages: []
-  }
-}
-
 app.post('/bloggers', (req: Request , res: Response) => {
   const postRequestErrors = errorFields();
-  console.log(req.body)
   let name = req.body.name 
-  // if(name === undefined || typeof name !== 'string' || name.trim() === '' || name.length > 40) {
     if(checkRequestBodyField(name, 'name')) {
-    // res.status(400).send({  
-    //   "errorsMessages": [
-    //     {
-    //       "message": "You did not send correct data",
-    //       "field": "name"
-    //     }
-    //   ]
-    // })
     const errorObj = {message: "You did not send correct data", field: "name"}
     postRequestErrors.errorsMessages.push(errorObj)
     }
     if(checkRequestBodyField(req.body.youtubeUrl, 'youtubeUrl')) {
-      // res.status(400).send({  
-      //   "errorsMessages": [
-      //     {
-      //       "message": "You did not send correct data",
-      //       "field": "youtubeUrl"
-      //     }
-      //   ]
-      // })
-      // return
       const errorObj = {message: "You did not send correct data", field: "youtubeUrl"}
     postRequestErrors.errorsMessages.push(errorObj)
     }
@@ -229,29 +222,11 @@ app.put('/bloggers/:id', (req: Request , res: Response) => {
     if(item.id === +req.params.id) {
       index = ind
     } return item.id === +req.params.id })
-  console.log(bloggerItem)
   if(checkRequestBodyField(req.body.name, 'name')) {
-    // res.status(400).send({  
-    //   "errorsMessages": [
-    //     {
-    //       "message": "You did not send correct data",
-    //       "field": "name"
-    //     }
-    //   ]
-    // })
     const errorObj = {message: "You did not send correct data", field: "name"}
     putRequestErrors.errorsMessages.push(errorObj)
     }
     if(checkRequestBodyField(req.body.youtubeUrl, 'youtubeUrl')) {
-      // res.status(400).send({  
-      //   "errorsMessages": [
-      //     {
-      //       "message": "You did not send correct data",
-      //       "field": "youtubeUrl"
-      //     }
-      //   ]
-      // })
-      // return
       const errorObj = {message: "You did not send correct data", field: "youtubeUrl"}
     putRequestErrors.errorsMessages.push(errorObj)
     }
@@ -260,19 +235,6 @@ app.put('/bloggers/:id', (req: Request , res: Response) => {
       return
     }
   if (bloggerItem) {
-    // let title = req.body.title;
-    // if(title === undefined || typeof title !== 'string' || title.trim() === '' || title.length > 40) {
-    //   console.log(req.body.title)
-    //   res.status(400).send({
-    //     "errorsMessages": [
-    //       {
-    //         "message": "You did not send correct data",
-    //         "field": "title"
-    //       }
-    //     ]
-    //   })
-    //   return
-    // }
     let newBloggers = bloggers.map((item, i) => {
       if(index === i) {
         item.name = req.body.name;
@@ -280,13 +242,130 @@ app.put('/bloggers/:id', (req: Request , res: Response) => {
       }
       return item
     })
-    bloggers = newBloggers;
-    console.log(bloggers)    
+    bloggers = newBloggers;  
     res.sendStatus(204).send('No Content');
   } else {
     res.sendStatus(404)
   }
 })
+////////////////////////////////////////////////
+
+app.get('/posts', (req, res) => {
+  res.status(200).send(posts);
+});
+
+app.get('/posts/:id', (req, res) => {
+  let postItem = posts.find(item => +item.id === +req.params.id);
+  if (postItem) {
+    let blogger = checkRequestBodyField(postItem.bloggerId.toString(), 'bloggerId')
+    if(!blogger){
+      res.status(200).send(postItem);
+    } else {
+      res.status(400).send('Bad Request')
+    }
+  } else {
+      res.sendStatus(404);
+  }
+});
+
+app.post('/posts', (req, res) => {
+  const postRequestErrors = errorFields();
+  if (checkRequestBodyField(req.body.title, 'title')) {
+      const errorObj = { message: "You did not send correct data", field: "title" };
+      postRequestErrors.errorsMessages.push(errorObj);
+  }
+  if (checkRequestBodyField(req.body.shortDescription, 'shortDescription')) {
+      const errorObj = { message: "You did not send correct data", field: "shortDescription" };
+      postRequestErrors.errorsMessages.push(errorObj);
+  }
+  if(checkRequestBodyField(req.body.content, 'content')) {
+      const errorObj = { message: "You did not send correct data", field: "content" };
+      postRequestErrors.errorsMessages.push(errorObj);
+  }
+  if(checkRequestBodyField(req.body.bloggerId, 'bloggerId')) {
+    const errorObj = { message: "You did not send correct data", field: "bloggerId" };
+    postRequestErrors.errorsMessages.push(errorObj);
+}
+  if (postRequestErrors.errorsMessages.length > 0) {
+      res.status(400).send({ postRequestErrors });
+      return;
+  }
+
+let blogger = bloggers.find(item => +item.id === +req.body.bloggerId)
+let newPost
+if(blogger) {
+  newPost = {
+      id: +(new Date()),
+      "title": req.body.title,
+      "shortDescription": req.body.shortDescription,
+      "content": req.body.content,
+      "bloggerId": req.body.bloggerId,
+      "bloggerName": blogger.name
+  };
+  posts.push(newPost);
+  res.status(201).send(newPost);
+}
+
+});
+
+app.put('/posts/:id', (req, res) => {
+  let index: number;
+  const putRequestErrors = errorFields();
+  let postItem = posts.find((item, ind) => {
+      if (item.id === +req.params.id) {
+          index = ind;
+      }
+      return +item.id === +req.params.id;
+  });
+  if (checkRequestBodyField(req.body.title, 'title')) {
+    const errorObj = { message: "You did not send correct data", field: "title" };
+    putRequestErrors.errorsMessages.push(errorObj);
+}
+if (checkRequestBodyField(req.body.shortDescription, 'shortDescription')) {
+    const errorObj = { message: "You did not send correct data", field: "shortDescription" };
+    putRequestErrors.errorsMessages.push(errorObj);
+}
+if(checkRequestBodyField(req.body.content, 'content')) {
+    const errorObj = { message: "You did not send correct data", field: "content" };
+    putRequestErrors.errorsMessages.push(errorObj);
+}
+if(checkRequestBodyField(req.body.bloggerId, 'bloggerId')) {
+    const errorObj = { message: "You did not send correct data", field: "bloggerId" };
+    putRequestErrors.errorsMessages.push(errorObj);
+}
+  if (putRequestErrors.errorsMessages.length > 0) {
+      res.status(400).send({ putRequestErrors });
+      return;
+  }
+  if (postItem) {
+      posts = posts.map((item, i) => {
+          if (index === i) {
+              item.title = req.body.title
+              item.shortDescription = req.body.shortDescription;
+              item.content = req.body.content;
+              item.bloggerId = req.body.bloggerId
+          }
+          return item;
+      });
+      res.sendStatus(204).send('No Content');
+  }
+  else {
+      res.sendStatus(404);
+  }
+});
+
+app.delete('/posts/:id', (req, res) => {
+  let length = posts.length;
+  posts = posts.filter(item => {
+      return item.id !== Number.parseInt(req.params.id);
+  });
+  if (length > posts.length) {
+      res.send(204);
+  } else {
+      res.send(404);
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
